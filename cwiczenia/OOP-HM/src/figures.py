428b4a65b3115ca535d3e2e66e716f20f595f82c
Michal Zbysinski <michal.zbysinski@ing.com>
2023-11-09 22:54:55 +0100

    Dodanie klasy test_figures.py

--- cwiczenia/OOP-HM/src/figures.py
+++ cwiczenia/OOP-HM/src/figures.py
@@ -1,22 +1,22 @@
 import math
 from abc import ABC, abstractmethod, abstractproperty
+import decimal
 
-PI = 3.14159
+PI = round(math.pi, 2)
 
 
 class Figure(ABC):
-
     @abstractmethod
-    def get_area(self)->object:
+    def get_area(self) -> object:
         """
         Abstrakcyjna funkcja zwracajaca pole figury
 
         :returns: object
-        """ 
+        """
         pass
 
     @abstractmethod
-    def get_perimeter(self)->object:
+    def get_perimeter(self) -> object:
         """
         Abstrakcyjna funkcja zwracajaca obwód figury
 
@@ -34,19 +34,19 @@ class Figure(ABC):
         pass
 
     @abstractproperty
-    def area(self) -> None:
+    def area(self):
         """Property dla atrybutu area"""
         pass
 
     @abstractproperty
-    def perimeter(self) -> None:
+    def perimeter(self):
         """Property dla atrybutu perimeter"""
         pass
 
     def raise_type_error(self):
         """Funkcja podnosząca wyjątek, gdy obiekt nie jest instancją klasy Figure"""
         raise TypeError("Obiekty muszą być instancją klasy Circle bądź Square")
-    
+
     def __eq__(self, other: object) -> bool:
         """
         Funkcja weryfikuje czy instancje klasy Figure są sobie równe
@@ -56,11 +56,13 @@ class Figure(ABC):
         """
 
         if isinstance(other, Figure):
-            return self.area == other.area
-        
+            print(self.area)
+            print(other.area)
+            return round(self.area, 2) == round(other.area, 2)
+
         else:
             self.raise_type_error()
-    
+
     def __ne__(self, other: object) -> bool:
         """
         Funkcja weryfikuje czy instancje klasy Figure nie są sobie równe
@@ -69,11 +71,11 @@ class Figure(ABC):
         :returns: bool
         """
         if isinstance(other, Figure):
-            return self.area != other.area
-        
+            return round(self.area, 2) != round(other.area, 2)
+
         else:
             self.raise_type_error()
-    
+
     def __lt__(self, other: object) -> bool:
         """
         Funkcja weryfikuje czy pole instancji klasy Figure jest mniejszy niż inny obiekt tej samej klasy
@@ -82,11 +84,11 @@ class Figure(ABC):
         :returns: bool
         """
         if isinstance(other, Figure):
-            return self.area < other.area
+            return round(self.area, 2) < round(other.area, 2)
 
         else:
             self.raise_type_error()
-    
+
     def __le__(self, other: object) -> bool:
         """
         Funkcja weryfikuje czy pole instancji klasy Figure jest mniejszy bądź równe niż inny obiekt tej samej klasy
@@ -95,11 +97,11 @@ class Figure(ABC):
         :returns: bool
         """
         if isinstance(other, Figure):
-            return self.area <= other.area
+            return round(self.area, 2) <= round(other.area, 2)
 
         else:
             self.raise_type_error()
-    
+
     def __gt__(self, other: object) -> bool:
         """
         Funkcja weryfikuje czy pole instancji klasy Figure jest większe niż inny obiekt tej samej klasy
@@ -108,7 +110,7 @@ class Figure(ABC):
         :returns: bool
         """
         if isinstance(other, Figure):
-            return self.area > other.area
+            return round(self.area, 2) > round(other.area, 2)
 
         else:
             self.raise_type_error()
@@ -121,25 +123,39 @@ class Figure(ABC):
         :returns: bool
         """
         if isinstance(other, Figure):
-            return self.area >= other.area
+            return round(self.area, 2) >= round(other.area, 2)
 
         else:
             self.raise_type_error()
-    
+
     def __add__(self, other) -> object:
         """
         Funkcja dodaje pola kół klasy circle tworzy nowy obiekt tej klasy wyliczajac nowy promien
 
-        :other: Circle obiekt klasy Circle
+        :other: Circle obiekt klasy Circle bad
         :returns: nowy obiekt klasy Circle
         """
-        if isinstance(other, Circle):
-            return Circle(math.sqrt((self.area + other.area) / PI))
-        elif isinstance(other, Square):
-            return Square((math.sqrt(self.area + other.area)))
-        else:
+        
+        if isinstance(self, Circle):
+            if isinstance(other, Figure):
+                return Circle(math.sqrt((self.area + other.area) / PI))
+            else:
+                self.raise_type_error()
+        elif isinstance(self, Square):
+            if isinstance(other, Figure):
+                return Square(math.sqrt(self.area + other.area))
             self.raise_type_error()
+    
+    def __radd__(self, other) -> object:
+        """
+        Funkcja wywołuje funkcję raise_type_error() w sytuacji gdy nastąpi próba dodania do Obiektu klasy Figure innego obiektu niebędącego instancją klasy Figure
 
+        :other: ANY int, str, float etc.
+        :returns: NONE
+        """
+        if not isinstance(other, Figure):
+             self.raise_type_error()   
+        
 class Circle(Figure):
     """Circle class jest wykorzystywana do przechowywania danych klasy Circle.
 
@@ -150,7 +166,7 @@ class Circle(Figure):
         __repr__()
     """
 
-    def __init__(self, radius=1) -> None:
+    def __init__(self, radius=1):
         """Circle Class Konstructor wykorzystywany do inicjalizacji obiektu.
 
         Input Arguments: radius - int | float, wartość domyślna 1
@@ -160,71 +176,79 @@ class Circle(Figure):
                 f"Promien kola nie moze byc ujemny lub wprowadzona wartosc to nie int albo float {radius}"
             )
         else:
-            self._radius = radius
+            self._radius = round(radius, 2)
 
     @property
-    def radius(self) -> (int or float):
+    def radius(self):
         """int | float: Property dla atrybutu _radius"""
         return self._radius
 
     @radius.setter
-    def radius(self, value:(int or float)) -> None:
+    def radius(self, value):
         """int | float: Property dla atrybutu _radius"""
         if not (isinstance(value, (int, float)) and value > 0):
-            raise ValueError("Promień kola musi być większy od zera, lub wprowadzona wartosc to nie int albo float, podana wartość: {value}")
-        self._radius = value
+            raise ValueError(
+                "Promień kola musi być większy od zera, podana wartość: {value}"
+            )
+        self._radius = round(value, 2)
 
     @property
-    def diameter(self) -> (int or float):
+    def diameter(self):
         """int | float: Property diameter wynosząca
         dwykrotność atrybutu _radius"""
         return 2 * self._radius
 
     @diameter.setter
-    def diameter(self, value:(int or float)) -> None:
+    def diameter(self, value):
         """int | float: Property diameter aktualizująca
         wartość atrybutu _radius"""
         if not (isinstance(value, (int, float)) and value > 0):
-            raise ValueError("Obwód kola musi być większy od zera, lub wprowadzona wartosc to nie int albo float, podana wartość: {value}")
-        self._radius = value / 2
+            raise ValueError(
+                "Obwód kola musi być większy od zera, podana wartość: {value}"
+            )
+        self._radius = round((value / 2), 2)
 
     @property
-    def area(self)-> float:
+    def area(self):
         """int | float: Property area zwracająca
         pole powierzchi instancji Circle"""
-        return PI * self._radius**2
+        return round(PI * self._radius**2, 2)
 
     @area.setter
-    def area(self, value:(int or float)) -> None:
+    def area(self, value):
         """int | float: Property area aktualizująca
         wartość atrybutu _radius"""
         if not (isinstance(value, (int, float)) and value > 0):
-            raise ValueError(f"Powierzchnia kola być większa od zera, lub wprowadzona wartosc to nie int albo float, podana wartość: {value}")
-        self._radius = math.sqrt((value) / 3.14)
-    
+            raise ValueError(
+                f"Powierzchnia kola być większa od zera, podana wartość: {value}"
+            )
+        self._radius = math.sqrt((value) / PI)
+
     @property
-    def perimeter(self) -> float:
+    def perimeter(self):
         """int | float: Property area zwracająca
         pole powierzchi instancji Circle"""
-        return 2*PI*self._radius
-    
+        return 2 * PI * self._radius
+
     @perimeter.setter
-    def perimeter(self,value:(int or float)) -> None:
+    def perimeter(self, value):
         """int | float: Property perimeter aktualizująca
         wartość atrybutu _radius"""
         if not (isinstance(value, (int, float)) and value > 0):
-            raise ValueError("Obwód kola musi być większy od zera, lub wprowadzona wartosc to nie int albo float, podana wartość: {value}")
-        self._radius = value/(2*PI)
-    
-    def get_perimeter(self) -> float:
+            raise ValueError(
+                "Obwód kola musi być większy od zera, podana wartość: {value}"
+            )
+        self._radius = round(value / (2 * PI), 2)
+
+    def get_perimeter(self):
         """
         Funkcja zwraca obwód koła
 
         :returns: int | float
         """
         return self.perimeter
-    
-    def get_area(self) -> float:
+
+    def get_area(self):
         """
         Funkcja zwraca pole koła
 
@@ -237,6 +261,7 @@ class Circle(Figure):
         reprezentacja instacji klasy Circle"""
         return f"Okrąg o promieniu {round(self._radius ,2)}, średnicy {round(self.diameter,2)} i polu powierzchni {round(self.area,2)}"
 
+
 class Square(Figure):
     """Square class jest wykorzystywana do przechowywania danych klasy Square.
 
@@ -246,59 +271,78 @@ class Square(Figure):
         get_area()
         __repr__()
     """
-    def __init__(self, side:(int or float)) -> None:
+
+    def __init__(self, side):
         """Square Class Konstructor wykorzystywany do inicjalizacji obiektu.
 
         Input Arguments: side - int | float
         """
         if not (isinstance(side, (int, float)) and side > 0):
             raise ValueError(
-                f"Bok kwadratu musi być większy od zera, lub wprowadzona wartosc to nie int albo float, podana wartość: {side}"
+                f"Bok kwadratu musi być większy od zera, podana wartość: {side}"
             )
         else:
             self._side = side
 
     @property
-    def area(self)-> (int or float):
+    def area(self):
         """int | float: Property area zwracająca
         pole powierzchi instancji Square"""
-        return self._side**2
+        return round(self._side**2, 2)
 
     @area.setter
-    def area(self, value:(int or float)) -> None:
+    def area(self, value):
         """int | float: Property area aktualizująca
         wartość atrybutu _side"""
         if not (isinstance(value, (int, float)) and value > 0):
-            raise ValueError(f"Powierzchnia kwadratu musi być większy od zera, lub wprowadzona wartosc to nie int albo float, podana wartość: {value}")
+            raise ValueError(
+                f"Powierzchnia kwadratu musi być większy od zera, podana wartość: {value}"
+            )
         self._side = math.sqrt(value)
 
     @property
-    def perimeter(self) -> (int or float):
+    def side(self):
+        """int | float: Property dla atrybutu _side"""
+        return self._side
+
+    @side.setter
+    def side(self, value):
+        """int | float: Property dla atrybutu _side"""
+        if not (isinstance(value, (int, float)) and value > 0):
+            raise ValueError(
+                "Promień kola musi być większy od zera, podana wartość: {value}"
+            )
+        self._side = round(value, 2)
+
+    @property
+    def perimeter(self):
         """
         int | float: Property area zwracająca
         obwód instancji Square
 
         :returns: Square
         """
-        return 4*self._side
-    
+        return 4 * self._side
+
     @perimeter.setter
-    def perimeter(self,value:(int or float))->None:
+    def perimeter(self, value):
         """int | float: Property perimeter aktualizująca
         wartość atrybutu _side"""
         if not (isinstance(value, (int, float)) and value > 0):
-            raise ValueError(f"Obwód kwadratu musi być większy od zera, lub wprowadzona wartosc to nie int albo float, podana wartość: {value}")
-        self._side = value/4
-    
-    def get_perimeter(self) -> (int or float):
+            raise ValueError(
+                f"Obwód kwadratu musi być większy od zera, podana wartość: {value}"
+            )
+        self._side = value / 4
+
+    def get_perimeter(self):
         """
         Funkcja zwraca obwód Kwadratu
 
         :returns: int | float
         """
         return self.perimeter
-    
-    def get_area(self) -> (int or float):
+
+    def get_area(self):
         """
         Funkcja zwraca pole kwadratu
 
@@ -306,19 +350,24 @@ class Square(Figure):
         """
         return self.area
 
-
     def __repr__(self) -> str:
         """str: Funkcja przedstawiająca tekstową
         reprezentacja instacji klasy Square"""
         return f"Kwadrat o boku {round(self._side ,2)} i polu powierzchni {round(self.area,2)}"
 
-if __name__ == "__main__":
-    s1 = Square(2)
-    c = Circle(1)
-    s1.area = 'gg'
-    print(c)
-    print(s1.get_area())
-    print(type(c))
-    print(s1+c)
-#print(s1==s2)    #print(s1.area)
 
+if __name__ == "__main__":
+ 
+    s1 = Square(4)
+    c1 = Square(2)
+    print(33+s1)
+
+#    import decimal
+## input floating-point number
+#    inputNumber = 4.140000000000001 
+## Converting the given number to decimal
+#    decimalValue = decimal.Decimal(inputNumber)
+## rounding the number upto 2 digits after the decimal point
+#    roundedNumber = decimalValue.quantize(decimal.Decimal('0.00'))
+#    print(roundedNumber)
+#    print("Rounding 4.140000000000001 upto 2 decimal places:", format(f3,".2f"))
---
+++ cwiczenia/OOP-HM/tests/test_figures.py
@@ -0,0 +1,462 @@
+import pytest
+import sys
+import math
+
+sys.path.append("../")
+from src.figures import Figure, Circle, Square
+
+
+PI = round(math.pi, 2)
+
+
+def test_figure__eq__():
+    """
+    Funkcja testujaca dla funkcji __eq__() z klasy Figure
+
+    :args: NONE
+    :returns: NONE
+    """
+    c1 = Circle(2)
+    c2 = Circle(2)
+    assert c1 == c2
+
+    s1 = Square(2)
+    s2 = Square(2)
+    assert s1 == s2
+
+    with pytest.raises(TypeError):
+        s1 == 2
+
+    with pytest.raises(TypeError):
+        c2 == "ssss"
+
+    c3 = Circle()
+    s3 = Square(2)
+    c3.area = 1
+    s3.area = 1
+
+    assert c3 == s3
+    assert s3 == c3
+
+
+def test_figure__ne__():
+    """
+    Funkcja testujaca dla funkcji __ne__() z klasy Figure
+
+    :args: NONE
+    :returns: NONE
+    """
+    c1 = Circle(1)
+    c2 = Circle(2)
+    assert c1 != c2
+
+    s1 = Square(1)
+    s2 = Square(2)
+    assert s1 != s2
+
+    with pytest.raises(TypeError):
+        s1 == 2
+
+    with pytest.raises(TypeError):
+        c2 == "ssss"
+
+    c3 = Circle()
+    s3 = Square(2)
+    c3.area = 2
+    s3.area = 1
+
+    assert c3 != s3
+    assert s3 != c3
+
+
+def test_figure__lt__():
+    """
+    Funkcja testujaca dla funkcji __lt__() z klasy Figure
+
+    :args: NONE
+    :returns: NONE
+    """
+    c1 = Circle(1)
+    c2 = Circle(2)
+    assert c1 < c2
+
+    s1 = Square(1)
+    s2 = Square(2)
+    assert s1 < s2
+
+    with pytest.raises(TypeError):
+        s1 < 2
+
+    with pytest.raises(TypeError):
+        c2 < "ssss"
+
+    c3 = Circle()
+    s3 = Square(2)
+    c3.area = 1
+    s3.area = 10
+
+    assert c3 < s3
+
+
+def test_figure__le__():
+    """
+    Funkcja testujaca dla funkcji __le__() z klasy Figure
+
+    :args: NONE
+    :returns: NONE
+    """
+    c1 = Circle(1)
+    c2 = Circle(2)
+    assert c1 <= c2
+
+    c1 = Circle(1)
+    c2 = Circle(1)
+    assert c1 <= c2
+
+    s1 = Square(1)
+    s2 = Square(2)
+    assert s1 <= s2
+
+    s1 = Square(2)
+    s2 = Square(2)
+    assert s1 <= s2
+
+    with pytest.raises(TypeError):
+        s1 <= 2
+
+    with pytest.raises(TypeError):
+        c2 <= "ssss"
+
+    c3 = Circle()
+    s3 = Square(2)
+    c3.area = 1
+    s3.area = 10
+
+    assert c3 < s3
+
+
+def test_figure__gt__():
+    """
+    Funkcja testujaca dla funkcji __gt__() z klasy Figure
+
+    :args: NONE
+    :returns: NONE
+    """
+    c1 = Circle(2)
+    c2 = Circle(1)
+    assert c1 > c2
+
+    s1 = Square(20)
+    s2 = Square(13)
+    assert s1 > s2
+
+    with pytest.raises(TypeError):
+        s1 > 2
+
+    with pytest.raises(TypeError):
+        c2 > "ssss"
+
+    c3 = Circle()
+    s3 = Square(2)
+    c3.area = 10
+    s3.area = 4
+
+    assert c3 > s3
+
+    c3.area = 4
+    s3.area = 10
+
+    assert s3 > c3
+
+
+def test_figure__ge__():
+    """
+    Funkcja testujaca dla funkcji __ge__() z klasy Figure
+
+    :args: NONE
+    :returns: NONE
+    """
+    c1 = Circle(2)
+    c2 = Circle(1)
+    assert c1 >= c2
+
+    c1 = Circle(2)
+    c2 = Circle(2)
+    assert c1 >= c2
+
+    s1 = Square(20)
+    s2 = Square(13)
+    assert s1 >= s2
+
+    s1 = Square(13)
+    s2 = Square(13)
+    assert s1 >= s2
+
+    with pytest.raises(TypeError):
+        s1 >= 2
+
+    with pytest.raises(TypeError):
+        c2 >= "ssss"
+
+    c3 = Circle()
+    s3 = Square(2)
+    c3.area = 10
+    s3.area = 4
+
+    assert c3 >= s3
+
+    c3.area = 10
+    s3.area = 10
+
+    assert s3 >= c3
+
+
+def test_figure__add__():
+    """
+    Funkcja testujaca dla funkcji __add__() z klasy Figure
+
+    :args: NONE
+    :returns: NONE
+    """
+    c1 = Circle()
+    c2 = Circle(2)
+    c3 = c1 + c2
+    assert c3.radius == 2.24
+    assert c3.diameter == 4.48
+    assert c3.area == 15.76
+
+    s1 = Square(4)
+    s2 = Square(2)
+    s3 = s1 + s2
+    assert s3.area == 20.00
+
+    s1 = Square(1)
+    c1 = Circle(1)
+    f3 = s1 + c1
+    print(type(f3))
+    assert f3.area == 4.14
+    
+    s1 = Square(1)
+    with pytest.raises(TypeError):
+        s1 + 2
+    
+    with pytest.raises(TypeError):
+        'sss' + s1
+
+
+
+
+def test_square_init():
+    """
+    Funkcja testujaca dla konstruktora __init__() z klasy Square
+
+    :args: NONE
+    :returns: NONE
+    """
+    s = Square(3)
+    assert s._side == 3
+
+    with pytest.raises(ValueError):
+        Square(0)
+
+    with pytest.raises(ValueError):
+        Square("sss")
+
+
+def test_square_side():
+    """
+    Funkcja testujaca dla dekoratora side z klasy Square
+
+    :args: NONE
+    :returns: NONE
+    """
+    s = Square(3)
+    s.side = 3
+    assert s._side == 3
+    assert s.area == 9
+
+    with pytest.raises(ValueError):
+        s.side = -3
+
+    with pytest.raises(ValueError):
+        s.side = "aaa"
+
+
+def test_square_perimeter():
+    """
+    Funkcja testujaca dla dekoratora perimeter z klasy Square
+
+    :args: NONE
+    :returns: NONE
+    """
+    s = Square(3)
+    assert s.perimeter == 12
+
+    with pytest.raises(ValueError):
+        s.perimeter = -2
+
+    with pytest.raises(ValueError):
+        s.perimeter = "aaa"
+
+
+def test_square_get_perimeter():
+    """
+    Funkcja testujaca dla funkcji get_perimeter() z klasy Square
+
+    :args: NONE
+    :returns: NONE
+    """
+    s = Square(3)
+    assert s.get_perimeter() == 12
+
+
+def test_square_get_area():
+    """
+    Funkcja testujaca dla funkcji get_area() z klasy Square
+
+    :args: NONE
+    :returns: NONE
+    """
+    s = Square(3)
+    assert s.get_area() == 9
+
+
+def test_square__repr__():
+    """
+    Funkcja testujaca dla funkcji __repr__() z klasy Square
+
+    :args: NONE
+    :returns: NONE
+    """
+    s = Square(3)
+    assert s.__repr__() == "Kwadrat o boku 3 i polu powierzchni 9"
+
+
+def test_circle___init__():
+    """
+    Funkcja testujaca dla konstruktora __init__() z klasy Circle
+
+    :args: NONE
+    :returns: NONE
+    """
+    c = Circle()
+    assert c.radius == 1
+
+    with pytest.raises(ValueError):
+        Circle(0)
+
+    with pytest.raises(ValueError):
+        Circle("sss")
+
+
+def test_circle_radius():
+    """
+    Funkcja testujaca dla property radius z klasy Circle
+
+    :args: NONE
+    :returns: NO
+    """
+    c = Circle()
+    assert c._radius == 1
+
+    c.radius = 3
+    assert c._radius == 3
+    assert c.area == PI * 9
+
+    with pytest.raises(ValueError):
+        c.radius = -3
+
+    with pytest.raises(ValueError):
+        c.radius = "aaa"
+
+
+def test_circle_diameter():
+    """
+    Funkcja testujaca dla property diameter z klasy Circle
+
+    :args: NONE
+    :returns: NO
+    """
+    c = Circle()
+    assert c.diameter == 2
+
+    c.diameter = 4
+    assert c._radius == 2
+
+    with pytest.raises(ValueError):
+        c.diameter = -2
+
+    with pytest.raises(ValueError):
+        c.diameter = "aaa"
+
+
+def test_circle_area():
+    """
+    Funkcja testujaca dla property area z klasy Circle
+
+    :args: NONE
+    :returns: NO
+    """
+    c = Circle(2)
+    assert c.area == 4 * PI
+    assert c._radius == 2
+
+    with pytest.raises(ValueError):
+        c.area = -2
+
+    with pytest.raises(ValueError):
+        c.area = "aaa"
+
+
+def test_circle_perimeter():
+    """
+    Funkcja testujaca dla property perimeter z klasy Circle
+
+    :args: NONE
+    :returns: NO
+    """
+    c = Circle(2)
+    assert c.perimeter == 4 * PI
+
+    with pytest.raises(ValueError):
+        c.perimeter = -2
+
+    with pytest.raises(ValueError):
+        c.perimeter = "aaa"
+
+
+def test_circle_get_perimeter():
+    """
+    Funkcja testujaca dla funkcji get_perimeter z klasy Circle
+
+    :args: NONE
+    :returns: NO
+    """
+    c = Circle(2)
+    assert c.get_perimeter() == 4 * PI
+
+
+def test_circle_get_area():
+    """
+    Funkcja testujaca dla funkcji get_perimeter z klasy Circle
+
+    :args: NONE
+    :returns: NO
+    """
+    c = Circle(2)
+    assert c.get_area() == 4 * PI
+
+
+def test_circle__repr__():
+    """
+    Funkcja testujaca dla funkcji __repr__() z klasy Circle
+
+    :args: NONE
+    :returns: NONE
+    """
+    c = Circle(2)
+    assert c.__repr__() == "Okrąg o promieniu 2, średnicy 4 i polu powierzchni 12.56"
+
+
+if __name__ == "__main__":
+    pytest.main()
